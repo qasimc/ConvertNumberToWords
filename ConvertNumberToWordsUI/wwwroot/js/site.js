64af9a7036449b7e12bfa94e49649f4e49edea48
Qasim Chaudhry <qasim@ayudasystems.com>
2020-06-25 10:07:03 +1000

    Add notification message

--- ConvertNumberToWordsUI/wwwroot/js/site.js
+++ ConvertNumberToWordsUI/wwwroot/js/site.js
@@ -10,14 +10,26 @@
         data: JSON.stringify(numberValue),
         success: function (data) {
             $("#msg").text(data);
+            ShowSuccess("Successfully converted and saved to database");
         },
         error: function (data, status, error) {
-            debugger;
             $("#msg").text(data.responseText);
+            ShowError(data.responseText);
         },
         complete: function (data) {
             $("#convert").text("Convert and save to database");
             $("#convert").removeAttr('disabled');
         }
     })
+}
+function ShowSuccess(message) {
+    $("#notification").text(message);
+    $("#notification").addClass("show");
+    setTimeout(function () { $("#notification").removeClass("show") }, 3000);
+}
+
+function ShowError(message) {
+    $("#notification").text(message);
+    $("#notification").addClass("showError");
+    setTimeout(function () { $("#notification").removeClass("showError") }, 3000);
 }
